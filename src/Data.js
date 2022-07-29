9b80e9dbf955d7adb140ee16c2fbfceca43ee0b3
Saber goob <saber.goob01@gmail.com>
2022-07-29 11:47:02 +0100

    frist commit

---
+++ src/Data.js
@@ -0,0 +1,68 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Trek Madone",
+    price: 800,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg",
+    ids: "f1"
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Bike 21PX",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg",
+    ids: "f2"
+
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Trek Madone",
+    price: 7000,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg",
+    ids: "f3"
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Trek SLR",
+    price: 8000,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg",
+    ids: "f4"
+  },
+  {
+    id: "thiecbawdjksadjk",
+    name: "Casque D47",
+    price: 200,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-5.jpg",
+    ids: "f5"
+  },
+  {
+    id: "iuertrywebncdjksadjk",
+    name: "Casual shoes",
+    price: 200,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-6.jpg",
+    ids: "f6"
+  },
+  {
+    id: "thierytbvcbvzdhadjk",
+    name: "black Y44",
+    price: 450,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-7.jpg",
+    ids: "f7"
+  },
+  {
+    id: "trfoiwfcnbcawdjksadjk",
+    name: "BIKE 784",
+    price: 700,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-8.jpg",
+    ids: "f8"
+  },
+  
+];
